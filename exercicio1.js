5da333b3c33ac6edfcfb73fff5fa141d060ec4f5
andrearnoni <andre_arnoni@yahoo.com.br>
2024-08-26 19:13:58 -0300

    instalada lib prompt-sync e executado exercício 1

---
+++ exercicio1.js
@@ -0,0 +1,8 @@
+const prompt = require("prompt-sync")();
+const numero = parseInt(prompt("Digite um número: "));
+
+if (numero % 2 === 0) {
+  console.log(`O número ${numero} é par`);
+} else {
+  console.log(`O número ${numero} é ímpar`);
+}
