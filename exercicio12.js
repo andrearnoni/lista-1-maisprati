bdbbc5083f9ab7a859ef9eeb6c4b7cca6ca10d5f
andrearnoni <andre_arnoni@yahoo.com.br>
2024-08-26 20:02:29 -0300

    executado exercicio 12

---
+++ exercicio12.js
@@ -0,0 +1,6 @@
+const prompt = require("prompt-sync")();
+const numero = parseInt(prompt("Digite um número: "));
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${numero} x ${i} = ${numero * i}`);
+}
